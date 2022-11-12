17296f39e88e8d36520254edf97686ad18e38e81
ahriknow <ahriknow@ahriknow.com>
2022-11-12 20:55:29 +0800

    ahriknow

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from "./Field/index.js"
 export * from "./Model/index.js"
 export * from "./types/index.js"
 
-export const version: string = '0.0.17'
+export const version: string = '0.1.0'
